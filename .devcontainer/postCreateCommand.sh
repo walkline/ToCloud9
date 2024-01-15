69f314f28497111a5387a40836b75bdf8229ea18
Vincent Moitrot <vincent.moitrot@gmail.com>
2024-01-15 21:40:24 +0000

    fix dependencies versions

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,5 @@
 go mod download
-go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.27.1
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.3
 go install golang.org/x/tools/cmd/stringer@latest
 export PATH="$PATH:$(go env GOPATH)/bin"
