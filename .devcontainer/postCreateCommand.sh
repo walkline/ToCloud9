b2990c7a5a266ac987aa5d88c2114a5df375f82f
Vincent Moitrot <vincent.moitrot@gmail.com>
2024-01-16 19:10:24 +0000

    fix protoc-gen-go version

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 go mod download
-go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.32
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.3
 go install golang.org/x/tools/cmd/stringer@latest
 export PATH="$PATH:$(go env GOPATH)/bin"
