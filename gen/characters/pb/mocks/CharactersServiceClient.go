fc6a836d0d672c23376b07212e8e30ae9ad6ae53
walkline <walkline.ua@gmail.com>
2025-02-20 09:33:35 +0100

    Fix tests by updating mock of character service client.

--- gen/characters/pb/mocks/CharactersServiceClient.go
+++ gen/characters/pb/mocks/CharactersServiceClient.go
@@ -182,6 +182,39 @@ func (_m *CharactersServiceClient) CharactersToLoginForAccount(ctx context.Conte
 	return r0, r1
 }
 
+// SavePlayerPosition provides a mock function with given fields: ctx, in, opts
+func (_m *CharactersServiceClient) SavePlayerPosition(ctx context.Context, in *pb.SavePlayerPositionRequest, opts ...grpc.CallOption) (*pb.SavePlayerPositionResponse, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *pb.SavePlayerPositionResponse
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, *pb.SavePlayerPositionRequest, ...grpc.CallOption) (*pb.SavePlayerPositionResponse, error)); ok {
+		return rf(ctx, in, opts...)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, *pb.SavePlayerPositionRequest, ...grpc.CallOption) *pb.SavePlayerPositionResponse); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*pb.SavePlayerPositionResponse)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, *pb.SavePlayerPositionRequest, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // ShortOnlineCharactersDataByGUIDs provides a mock function with given fields: ctx, in, opts
 func (_m *CharactersServiceClient) ShortOnlineCharactersDataByGUIDs(ctx context.Context, in *pb.ShortCharactersDataByGUIDsRequest, opts ...grpc.CallOption) (*pb.ShortCharactersDataByGUIDsResponse, error) {
 	_va := make([]interface{}, len(opts))
